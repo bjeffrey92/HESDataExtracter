11bb32bbb16dc5dbddaf7ec12fdc40165f97dc5a
bmjeffrey19 <bjeffrey92@hotmail.com>
2020-03-16 13:04:52 +0000

    wrote function to validate APC data file after being loaded and incorporated it into other loading functions

--- R/loadData.R
+++ R/loadData.R
@@ -1,5 +1,5 @@
 library(haven)
-
+library(stringr)
 
 #' Load Data from stata .dta file 
 #'
@@ -12,6 +12,8 @@ loadFromDta <- function(dtaFile, cols = NULL){
     
     data <- haven::read_dta(dtaFile)
     
+    validateFile(data)
+
     print('Filtering columns')
     cols <- c('startage','encrypted_hesid','admidate','disdate',
             'epistat','procode','procode3','soal') #relevant columns in APC data to extract
@@ -38,6 +40,8 @@ loadFromCsv <- function(csvFile, cols = NULL, sep = ','){
     data <- read.csv(csvFile, sep = sep, 
                     stringsAsFactors = FALSE)
     
+    validateFile(data)
+
     print('Filtering columns')
     cols <- c('startage','encrypted_hesid','admidate','disdate',
             'epistat','procode','procode3','soal') #relevant columns in APC data to extract
@@ -48,4 +52,40 @@ loadFromCsv <- function(csvFile, cols = NULL, sep = ','){
     df <- df[,names(df) %in% c(cols, "allDiagnoses")] #drop unnesecary columns
     
     return(df)
+}
+
+#' Validate File
+#'
+#' @description checks that file is in appropriate format for analysis with rest of functions in package
+#' @param data APC data loaded from file
+#' @param date.format format the dates must be in
+#' @param dateCols vector of column names that should incude only dates in format of date.format
+#' @param patterns vector of patterns that match other columns which are to be extracted
+#' @export
+validateFile <- function(data, date.format = '%y/%m/%d',
+                        dateCols = c('admidate', 'disdate'),
+                        patterns = c('diag_')){
+
+    #check date columns are in the correct format
+    #throw error if not
+    for (col in dateCols){
+        data[[col]] <- str_replace_all(data[[col]], '-', '/')
+        count <- 0
+        for (i in is.na(as.Date(data[[col]], date.format))){
+            if (i == FALSE){
+                count <- count + 1
+            }
+        }
+        if (count > 0){
+            stop(paste0(as.character(count), ' records in column ', col, ' are in incorrect format'))
+        }
+    }
+
+    #how many columns include pattern
+    for (pattern in patterns){
+        count <- sum(grepl(pattern, names(data)), na.rm = TRUE)
+        if (count == 0){
+            stop(paste0('No columns matching pattern: ', pattern))
+        }
+    }
 }
